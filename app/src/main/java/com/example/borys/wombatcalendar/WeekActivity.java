5c5ed103e3c68245c6b8e9721f247579e1340c68
mulciber55 <Mulciber55@gmail.com>
2015-10-08 11:40:58 +0200

    Move counting of weeks to Activity

--- app/src/main/java/com/example/borys/wombatcalendar/WeekActivity.java
+++ app/src/main/java/com/example/borys/wombatcalendar/WeekActivity.java
@@ -65,7 +65,8 @@ public class WeekActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return WeekFragment.newInstance(position);
+            int openWeek = position - MAX_PAGE / 2);
+            return WeekFragment.newInstance(openWeek);
         }
 
         @Override
