8b75bef320956c0ba2aa0057c044df7a77d70eb6
borys <borysstach@gmail.com>
2015-12-08 15:15:49 +0100

    Remove Recycler view for performace

--- app/src/main/java/com/example/borys/wombatcalendar/WeekFragment.java
+++ app/src/main/java/com/example/borys/wombatcalendar/WeekFragment.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -97,7 +97,8 @@ public class WeekFragment extends Fragment {
     public class DayOfWeekViewHolder extends RecyclerView.ViewHolder {
 
         private TextView mDayName;
-        private RecyclerView mDayRecyclerView;
+        private List<TextView> mEventViews = new ArrayList<>();
+        private TextView mEventNum;
         private LinearLayout mLinearLayout;
         private Calendar cloneCalendar;
 
@@ -105,20 +106,25 @@ public class WeekFragment extends Fragment {
             super(itemView);
             //find view
             mDayName = (TextView) itemView.findViewById(R.id.day_name_text_view);
+            mEventViews.add((TextView) itemView.findViewById(R.id.week_view_event_0));
+            mEventViews.add((TextView) itemView.findViewById(R.id.week_view_event_1));
+            mEventViews.add((TextView) itemView.findViewById(R.id.week_view_event_2));
+            mEventNum = (TextView) itemView.findViewById(R.id.week_view_event_num);
             //change color depend of month
-            String monthString = mMonthColorStrings.get(currentMonth)+"_day_title";
-            int thisMonthColor= ContextCompat.getColor(getActivity(), getActivity().getResources().getIdentifier(monthString, "color", getActivity().getPackageName()));
+            String monthString = mMonthColorStrings.get(currentMonth) + "_day_title";
+            int thisMonthColor = ContextCompat.getColor(getActivity(), getActivity().getResources().getIdentifier(monthString, "color", getActivity().getPackageName()));
             mDayName.setTextColor(thisMonthColor);
             //get stroke width
-            Resources r = getResources();
-            float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, r.getDisplayMetrics());
+            Resources resources = getResources();
+            float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, resources.getDisplayMetrics());
             int intPx = Math.round(px);
             //change stroke color depend of month
-            GradientDrawable strokeDrawable = (GradientDrawable) ContextCompat.getDrawable(getActivity() , getActivity().getResources().getIdentifier("stroke_shape", "drawable", getActivity().getPackageName()));
-            String monthColorString = mMonthColorStrings.get(currentMonth)+"_stroke";
-            strokeDrawable.setStroke(intPx ,ContextCompat.getColor(getActivity(), getActivity().getResources().getIdentifier(monthColorString, "color", getActivity().getPackageName())));
+            GradientDrawable strokeDrawable = (GradientDrawable) ContextCompat.getDrawable(getActivity(), getActivity().getResources().getIdentifier("stroke_shape", "drawable", getActivity().getPackageName()));
+            String monthColorString = mMonthColorStrings.get(currentMonth) + "_stroke";
+            strokeDrawable.setStroke(intPx, ContextCompat.getColor(getActivity(), getActivity().getResources().getIdentifier(monthColorString, "color", getActivity().getPackageName())));
             mLinearLayout = (LinearLayout) itemView.findViewById(R.id.day_linear_layout);
             mLinearLayout.setBackground(strokeDrawable);
+            //set
             TextView newActivityButton = (TextView) itemView.findViewById(R.id.day_button);
             newActivityButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -130,21 +136,11 @@ public class WeekFragment extends Fragment {
                     startActivity(intent);
                 }
             });
-            //find recycler view for Day Grid Item
-            mDayRecyclerView = (RecyclerView) itemView.findViewById(R.id.single_day_recyclerview);
-            mDayRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-            mDayRecyclerView.setAdapter(new SingleDayRecyclerAdapter(new ArrayList<EventData>()));
         }
 
         public void bindDay(String dayName) {
             //bind data to view
             mDayName.setText(dayName);
-        }
-
-        /**
-         * Set adapter on RecyclerView for Day Grid Item
-         */
-        public void setAdapter() {
             //get current calendar data to start activty intent
             cloneCalendar = (Calendar) mCalendar.clone();
             //new thread to get data from content provider
@@ -154,19 +150,33 @@ public class WeekFragment extends Fragment {
                     CalendarDataSource readerEvents = new CalendarDataSource(getContext());
                     final List<EventData> events = readerEvents.getEventsFromDay(cloneCalendar);
                     //to prevent null exeption
-                    if(getActivity() == null)
+                    if (getActivity() == null)
                         return;
                     //back to UI thread
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            //set adapter with all events this day
-                            mDayRecyclerView.setAdapter(new SingleDayRecyclerAdapter(events));
+                            //set data to views
+                            if (events.size() < 3) {
+                                for (int i = 0; i < events.size(); i++) {
+                                    mEventViews.get(i).setText(events.get(i).getTitle());
+                                    Log.d("UI", events.get(i).getTitle());
+                                }
+                            } else {
+                                for (int i = 0; i < 3; i++) {
+                                    mEventViews.get(i).setText(events.get(i).getTitle());
+                                }
+                                if ((events.size() - 3) > 0) {
+                                    mEventNum.setText("+" + (events.size() - 3));
+                                }
+                            }
+
                         }
                     });
                 }
             }).start();
         }
+
 
     }
 
@@ -186,16 +196,15 @@ public class WeekFragment extends Fragment {
         @Override
         public void onBindViewHolder(DayOfWeekViewHolder holder, int position) {
             //bind data from mDays list
-            if (position != 0){
+            if (position != 0) {
                 //add one day in next day after monday
-            mCalendar.add(Calendar.DAY_OF_WEEK, 1);
+                mCalendar.add(Calendar.DAY_OF_WEEK, 1);
             }
             //set number of day for this day of week
             int day = mCalendar.get(Calendar.DAY_OF_MONTH);
             String dayName = mDaysStrings.get(position);
             holder.bindDay(dayName + "  " + day);
             //make recyclerView with list of this day events
-            holder.setAdapter();
 
         }
 
@@ -204,65 +213,6 @@ public class WeekFragment extends Fragment {
             return mDaysStrings.size();
         }
     }
-
-
-
-    //////////////////////////////////////////   RecyclerView inside Day Grid Item  ///////////////////////////////////////
-
-
-    public class SingleDayViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView mEventTitle;
-
-
-        public SingleDayViewHolder(View itemView) {
-            super(itemView);
-            //find view
-            mEventTitle = (TextView) itemView.findViewById(R.id.single_day_textview);
-            //set color depend of month
-            String monthString = mMonthColorStrings.get(currentMonth)+"_just_text";
-            mEventTitle.setTextColor(ContextCompat.getColor(getActivity(), getActivity().getResources().getIdentifier(monthString, "color", getActivity().getPackageName())));
-        }
-
-
-        public void bindEvent(EventData event) {
-            mEventTitle.setText(event.getTitle());
-        }
-    }
-
-    public class SingleDayRecyclerAdapter extends RecyclerView.Adapter<SingleDayViewHolder> {
-        private List<EventData> mEvents;
-
-        public SingleDayRecyclerAdapter(List<EventData> events){
-            mEvents = events;
-        }
-
-        @Override
-        public SingleDayViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            //inflate layout to ViewHolder
-            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(R.layout.week_view_single_event, parent, false);
-
-            //return new ViewHolder with layout
-            return new SingleDayViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(SingleDayViewHolder holder, int position) {
-            //bind data
-            if (!mEvents.isEmpty()) {
-                EventData event = mEvents.get(position);
-                holder.bindEvent(event);
-            }
-        }
-
-
-    @Override
-    public int getItemCount() {
-        return mEvents.size();
-    }
-}
 
 
     /**
