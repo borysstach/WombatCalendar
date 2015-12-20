6713811c8b6c700bd6ecfa8cc7f68161249bf37c
borys <borysstach@gmail.com>
2015-12-20 20:51:01 +0100

    Working month show

--- app/src/main/java/com/stach/borys/wombatcalendar/MonthFragment.java
+++ app/src/main/java/com/stach/borys/wombatcalendar/MonthFragment.java
@@ -3,16 +3,27 @@ package com.stach.borys.wombatcalendar;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class MonthFragment extends Fragment {
-    static MonthFragment newInstance(int num) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
+public class MonthFragment extends Fragment {
+
+    private Integer numberOfDays;
+    private Calendar mMonth;
+    private List<String> mMonthStrings;
+
+    static MonthFragment newInstance(int num) {
         MonthFragment fragment = new MonthFragment();
         Bundle args = new Bundle();
         args.putInt("num", num);
@@ -20,14 +31,40 @@ public class MonthFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Integer numberOfFragment = getArguments().getInt("num");
+        mMonth = Calendar.getInstance();
+        mMonth.set(Calendar.DAY_OF_MONTH, 1);
+        mMonth.add(Calendar.MONTH, numberOfFragment);
+        numberOfDays = mMonth.getActualMaximum(Calendar.DAY_OF_MONTH);
+        mMonthStrings = new ArrayList<>(Arrays.asList(
+                getString(R.string.january),
+                getString(R.string.february),
+                getString(R.string.march),
+                getString(R.string.april),
+                getString(R.string.may),
+                getString(R.string.june),
+                getString(R.string.julay),
+                getString(R.string.august),
+                getString(R.string.september),
+                getString(R.string.october),
+                getString(R.string.november),
+                getString(R.string.december)));
+
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.month_fragment, container, false);
         RecyclerView monthRecyclerView = (RecyclerView) rootView.findViewById(R.id.month_recycler_view);
-
         monthRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 7));
         monthRecyclerView.setAdapter(new MonthRecyclerAdapter());
-
+        TextView monthTitle = (TextView) rootView.findViewById(R.id.month_title);
+        TextView yearTitle = (TextView) rootView.findViewById(R.id.month_year_title);
+        monthTitle.setText(mMonthStrings.get(mMonth.get(Calendar.MONTH)));
+        yearTitle.setText(""+mMonth.get(Calendar.YEAR));
         return rootView;
     }
 
@@ -36,21 +73,26 @@ public class MonthFragment extends Fragment {
     public class DayOfMonthViewHolder extends RecyclerView.ViewHolder {
 
         private TextView mDayNumber;
+        private FrameLayout mFrameLayout;
 
         public DayOfMonthViewHolder(View itemView) {
             super(itemView);
             mDayNumber = (TextView) itemView.findViewById(R.id.month_day_number);
+            mFrameLayout = (FrameLayout) itemView.findViewById(R.id.month_day_frame);
         }
 
-        public void bindDay(Integer position) {
-            mDayNumber.setText("" + position);
+        public void bindDay(Integer day) {
+            mDayNumber.setText("" + (day));
+            mFrameLayout.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.day_stroke_back));
         }
 
     }
+
     ////////////////////////////             ADAPTER
 
     public class MonthRecyclerAdapter extends RecyclerView.Adapter<DayOfMonthViewHolder> {
 
+        private Integer day = 1;
 
         @Override
         public DayOfMonthViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -62,12 +104,35 @@ public class MonthFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(DayOfMonthViewHolder holder, int position) {
-          holder.bindDay(position);
+            if(!(position < getDayOfWeekFromFirstDayOfMonth(mMonth))) {
+                holder.bindDay(day);
+                day++;
+            }
         }
 
         @Override
         public int getItemCount() {
-            return 36;
+            return numberOfDays + getDayOfWeekFromFirstDayOfMonth(mMonth);
         }
     }
+private Integer getDayOfWeekFromFirstDayOfMonth(Calendar firstDayOfMonth) {
+    switch (firstDayOfMonth.get(Calendar.DAY_OF_WEEK)) {
+        case Calendar.MONDAY:
+            return 0;
+        case Calendar.TUESDAY:
+            return 1;
+        case Calendar.WEDNESDAY:
+            return 2;
+        case Calendar.THURSDAY:
+            return 3;
+        case Calendar.FRIDAY:
+            return 4;
+        case Calendar.SATURDAY:
+            return 5;
+        case Calendar.SUNDAY:
+            return 6;
+        default:
+            return 0;
+    }
+}
 }
