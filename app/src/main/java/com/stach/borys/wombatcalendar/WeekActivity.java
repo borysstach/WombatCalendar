3250f4a7e26981ecef57f52112cd53ed3e37a640
borys <borysstach@gmail.com>
2015-12-21 02:13:46 +0100

    Back to today menu option

--- app/src/main/java/com/stach/borys/wombatcalendar/WeekActivity.java
+++ app/src/main/java/com/stach/borys/wombatcalendar/WeekActivity.java
@@ -275,18 +275,17 @@ public class WeekActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_month) {
             Intent intent = new Intent(this, MonthActivity.class);
             intent.putExtra(MONTH_INTENT, getCalendarFromPosition(mPosition).get(Calendar.MONTH));
             intent.putExtra(YEAR_INTENT, getCalendarFromPosition(mPosition).get(Calendar.YEAR));
             startActivity(intent);
             return true;
+        }
+        if (id == R.id.action_back_to_now) {
+           mViewPager.setCurrentItem(MAX_PAGE/2);
         }
 
         return super.onOptionsItemSelected(item);
